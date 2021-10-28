fa30ff991721d4c25f178abf29274dbaf0329bfb
Vlad Pupaza <vlad.pupaza@softvision.com>
2021-10-28 14:23:49 +0300

    feat(earlybirds): add MPP and SPP

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4dce670f-6b32-4b70-9463-ae7ff6fe067a'
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       colors: {
         primary: colors.indigo,
+        jmBg: '#fcf9ee'
       },
       container: {
         center: true,
